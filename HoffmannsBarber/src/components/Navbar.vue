7a1c872d9a63e763fdbc17e5795da20ed985c46f
MASTERDOSNOOBS <jairderioboeing@gmail.com>
2024-06-20 00:48:19 -0300

    up

--- HoffmannsBarber/src/components/Navbar.vue
+++ HoffmannsBarber/src/components/Navbar.vue
@@ -1,6 +1,15 @@
 <template>
     <nav>
-        <img class="logo" src="../assets/logo-blank.svg" alt="">
+        <a href="home">
+            <img class="logo" src="../assets/logo-blank.svg" >
+        </a>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Catálogo</a></li>
+            <li><a href="#">Planos</a></li>
+            <li><a href="#">Contato</a></li>
+        </ul>
+        <img src="../assets/menu-hamburguer.svg" class="menu-hamburguer">
     </nav>
 </template>
 
@@ -18,6 +27,28 @@ nav {
     background-color: #D9D9D9;
     position: sticky;
     top: 0px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.menu-hamburguer {
+    cursor: pointer;
+    display: none;
+}
+nav ul{
+    display: flex;
+    list-style: none;
+    align-items: center;
+}
+nav ul li{
+    padding: 0 40px 0 20px;
+    
+}
+nav ul li a{
+    text-decoration: none;
+    font-size: 18px;
+    color: black;
 }
 
 .logo {
