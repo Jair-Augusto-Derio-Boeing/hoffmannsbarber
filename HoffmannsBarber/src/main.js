0d4da9fe878a2e7b819fdea327e4436327baf714
VitorRBrumatti <vitorrufino084@gmail.com>
2024-09-19 20:26:34 -0300

    revert

--- HoffmannsBarber/src/main.js
+++ HoffmannsBarber/src/main.js
@@ -1,16 +1,11 @@
 import './assets/main.css'
 
-import Vue from 'vue'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 const app = createApp(App)
 
 app.use(router)
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
 app.mount('#app')
