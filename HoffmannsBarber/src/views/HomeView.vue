7394be155d8308a2573115513fb9105a1390ee63
VitorRBrumatti <vitorrufino084@gmail.com>
2024-09-19 20:12:10 -0300

    Install bootstrap-vue

--- HoffmannsBarber/src/views/HomeView.vue
+++ HoffmannsBarber/src/views/HomeView.vue
@@ -3,35 +3,33 @@
 
     <section class="about-me">
       <img src="../assets/Effects.png" class="effect" alt="Effects">
-<<<<<<< HEAD
+
       <img src="../assets/barber-pole.svg" class="barber-pole" alt="">
       <img src="../assets/barber-pole.svg" class="barber-pole2" alt="">
-      <div class="effect-lucas">
-        <img src="../assets/barber-scissor.svg" class="scissor" alt="">
-        <img src="../assets/barber-scissor.svg" class="scissor2" alt="">
-        <img class="lucas-backdrop" src="../assets/LucaoPhoto.png" alt="Lucas Hoffmann">
-=======
       <div class="center">
 
-        <div class="effect-lucas">
-          <img src="../assets/barber-scissor.svg" class="scissor" alt="">
-          <img src="../assets/barber-scissor.svg" class="scissor2" alt="">
-          <img class="lucas-backdrop" src="../assets/LucaoPhoto.png" alt="Lucas Hoffmann">
+        <div style="display: grid;">
+          <div class="effect-lucas">
+            <img src="../assets/barber-scissor.svg" class="scissor" alt="">
+            <img src="../assets/barber-scissor.svg" class="scissor2" alt="">
+            <img class="lucas-backdrop" src="../assets/LucaoPhoto.png" alt="Lucas Hoffmann">
+          </div>
+          <div class="p">
+            <h1>QUEM É LUCAS HOFFMANN?</h1>
+            <p>Lucas Hoffman, Barbeiro a mais de 3 anos, possui mais de 4 certificados no currículo, um profissional
+              capacitado para te atender!
+              Além de tudo isso, Lucas ministra seu próprio curso de barbeiro, indo do iniciante até técnicas avançadas!
+            </p>
+          </div>
         </div>
 
-        <div class="p">
-          <h1><b> QUEM É LUCAS HOFFMANN?</b></h1>
-          <p>Barbeiro a mais de 3 anos, mais de 4 certificados no currículo, um profissional capacitado para te atender!
-            Alem de tudo, professor para barbeiro! Ministro cursos do iniciante ate as técnicas avançadas !</p>
-        </div>
->>>>>>> a17cbc0a277e28d149b0ccee9f37bafd3e3abec5
       </div>
     </section>
     <section>
       <img src="../assets/Effect-invert.png" class="effect2" alt="Effects">
     </section>
     <Navbar v-model:NoneLogo="NoneLogo" @OpenMenu="OpenMenu"></Navbar>
-    <Menu v-model:ShowMenu="ShowMenu"></Menu>
+    <Menu v-model:showMenu="showMenu"></Menu>
   </div>
 
 
@@ -45,21 +43,16 @@ import Menu from '@/components/Menu.vue'
 export default {
   data() {
     return {
-      ShowMenu: false,
-      NoneLogo: true,
+      showMenu: false,
     }
   },
   methods: {
     OpenMenu() {
-      if (this.ShowMenu === true) {
-        this.ShowMenu = false;
-        this.NoneLogo = true;
-
+      if (this.showMenu === true) {
+        this.showMenu = false;
       }
       else {
-        this.ShowMenu = true;
-        this.NoneLogo = false;
-
+        this.showMenu = true;
       }
     },
   },
@@ -71,12 +64,17 @@ export default {
 </script>
 
 <style>
+html {
+  overflow-x: hidden;
+
+}
+
 .back {
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
-  min-height: 400vh;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
 }
@@ -104,80 +102,87 @@ export default {
 .effect-lucas {
   position: relative;
   left: 9%;
-  top: 15%;
-  width: 15%;
-  height: 50%;
+  width: 400px;
+  height: 450px;
+  bottom: 100px;
 }
 
 .lucas-backdrop {
-  position: relative;
   width: 100%;
   border-radius: 20px;
+
 }
 
 .scissor {
   width: 20%;
-  position: absolute;
-  top: 94%;
-  left: -10%;
+  position: relative;
+  top: 450px;
+  left: -35px;
 }
 
 .scissor2 {
   width: 20%;
-  position: absolute;
-  top: -6%;
-  left: 90%;
+  position: relative;
+  top: 35px;
+  left: 200px;
   rotate: 180deg;
 }
-<<<<<<< HEAD
+
 .barber-pole {
   position: absolute;
   left: -1%;
   transform: scaleX(-1);
 }
+
 .barber-pole2 {
   position: absolute;
-  right: 0%;
+  right: -1%;
   margin-right: 0;
-=======
+  bottom: 0;
+}
 
 .p {
+  display: grid;
   position: relative;
   color: #3E3E3E;
-  left: 35%;
-  top: 10%;
-  text-align: center;
+  text-align: justify;
+  width: 100%;
+  font-size: 25px;
+  justify-items: center;
+}
+
+.p p {
+  width: 50%;
 }
 
 .p h1 {
-  width: 50%;
   font-size: 42px;
   font-weight: 900;
 }
 
-.p p {
-  width: 30%;
-  left: 9.5%;
-  position: relative;
-  font-size: 120%;
+.grid {
+  display: flex;
+  align-items: center;
+  gap: 10%;
+  margin-top: 150px;
+  width: 100vw
 }
 
 @media(max-width: 970px) {
-
-  .about-me {
+  .grid {
     display: grid;
-  }
-  .p{
-    justify-content: center;
-  }
-.p h1{
-  font-size: 180%;
-}
-  .p p {
-    font-size: 100%;
+    z-index: 10;
+    justify-items: center;
   }
 
+  .effect-lucas {
+    width: 250px;
+    height: 300px;
+  }
 
->>>>>>> a17cbc0a277e28d149b0ccee9f37bafd3e3abec5
+  .center {
+    display: grid;
+    align-items: center;
+  }
 }
 </style>
